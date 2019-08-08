8210cba581f442a0fdd00642785da05c6547c4c2
Kashif Junaid <kashif.junaid@otwosoft.com>
2019-08-08 16:17:39 +0500

    server change

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.use(express.static(path.join(__dirname, '/')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
+  });
 app.use('/api/auth', authApi);
 app.use('/api/admin', adminApi);
 app.use('/api/config', configApi);
