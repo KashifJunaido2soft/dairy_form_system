8c45a74751bbf57e618cc895492b508a7cf99dc0
Kashif Junaid <kashif.junaid@otwosoft.com>
2019-08-27 19:00:53 +0500

    server.js change

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const authApi = require('./src/server/routes/auth');
 const adminApi = require('./src/server/routes/admin');
 const userApi = require('./src/server/routes/users');
 const accountsApi = require('./src/server/routes/accounts');
+const saleApi = require('./src/server/routes/sale');
 const purchaseApi = require('./src/server/routes/purchase');
 const configApi = require('./src/server/routes/config');
+const dashboardApi = require('./src/server/routes/dashboard');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -18,6 +20,7 @@ var wss = new WebSocketServer({ port: 1337 });
 wss.on('connection', function (w, req) {
 
   w.on('message', function (msg) {
+    // console.log(msg);
     wss.broadcast(msg, w);
   });
 
@@ -33,11 +36,10 @@ wss.on('connection', function (w, req) {
   wss.broadcast = function broadcast(msg, sender) {
 
     wss.clients.forEach(function each(client) {
-      //console.log(msg);
-      //console.log(client);
+      // console.log(client);
       // var data = JSON.parse(msg);
       var data = '"' + msg + '"';
-      console.log(data);
+      // console.log(data);
       // if (client !== sender) {
       client.send(data);
       // }
@@ -49,19 +51,23 @@ app.use(express.static(path.join(__dirname, '/')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.use(function (req, res, next) {
   //Enabling CORS
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
   next();
-  });
+
+});
 app.use('/api/auth', authApi);
+app.use('/api/dashboard', dashboardApi);
 app.use('/api/admin', adminApi);
 app.use('/api/config', configApi);
 app.use('/api/users', userApi);
 app.use('/api/accounts', accountsApi);
 app.use('/api/purchase', purchaseApi);
+app.use('/api/sale', saleApi);
 app.listen(port, function () {
   console.log("server running on localhost: " + port);
 });
